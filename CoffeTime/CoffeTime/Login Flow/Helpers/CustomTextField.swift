232f41efe935f88947a0641bd680a0b352845673
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-26 10:31:47 +0300

    create CustomTextField

---
+++ CoffeTime/CoffeTime/Login Flow/Helpers/CustomTextField.swift
@@ -0,0 +1,61 @@
+//
+//  CustomTextField.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 26.06.2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var sfIcon: String
+    var iconTint: Color = .gray
+    var hint: String
+    /// hides TextField
+    var isPassword: Bool = false
+    @Binding var value: String
+
+    /// View properties
+    @State private var showPassword: Bool = false
+
+    var body: some View {
+        HStack(alignment: .top, spacing: 8) {
+            Image(systemName: sfIcon)
+                .foregroundStyle(iconTint)
+                .frame(width: 30)
+
+            VStack(alignment: .leading, spacing: 8) {
+                if isPassword {
+                    Group {
+                        /// revealing password when users wants to show password
+                        if showPassword {
+                            TextField(hint, text: $value)
+                        } else {
+                            SecureField(hint, text: $value)
+                        }
+                    }
+                } else {
+                    TextField(hint, text: $value)
+                }
+
+                Divider()
+            }
+            .overlay(alignment: .trailing) {
+                Button {
+                    withAnimation {
+                        showPassword.toggle()
+                    }
+                } label: {
+                    Image(systemName: showPassword ? "eye.slash" : "eye")
+                        .foregroundStyle(.gray)
+                        .padding(10)
+                        .contentShape(.rect)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CustomTextField(sfIcon: "person", hint: "hint", value: .constant("test"))
+}
