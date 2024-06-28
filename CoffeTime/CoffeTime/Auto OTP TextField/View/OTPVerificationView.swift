9025add7e712a265c9d5781c576f7a56c53279c0
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-28 15:07:00 +0300

    implement OTPTextBox

--- CoffeTime/CoffeTime/Auto OTP TextField/View/OTPVerificationView.swift
+++ CoffeTime/CoffeTime/Auto OTP TextField/View/OTPVerificationView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct OTPVerificationView: View {
 
     @State var otpText: String = ""
+    private var otpDigitsCount = 6
 
     var body: some View {
         VStack {
             Text("Verify OTP")
                 .font(.largeTitle.bold())
                 .frame(maxWidth: .infinity, alignment: .leading)
+
+
+            HStack(spacing: 0) {
+                ForEach(0..<otpDigitsCount, id:\.self) { index in
+                    OTPTextBox(index: index, otpTextCount: otpDigitsCount)
+                }
+            }
+            .padding(.bottom, 20)
+            .padding(.top, 10)
 
             TextField("", text: $otpText)
 
@@ -32,6 +42,7 @@ struct OTPVerificationView: View {
                             .fill(.blue)
                     }
             }
+            .disableWithOpacity(otpText.count < 6)
         }
         .padding(.all)
         .frame(maxHeight: .infinity, alignment: .top)
