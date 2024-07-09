569e7bb41b467eb8ab3a31eec9741c6cbfba3612
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-09 19:32:50 +0300

    add overlay window

--- CoffeTime/CoffeTime/Universal hero effect/jHelpers/Hero.swift
+++ CoffeTime/CoffeTime/Universal hero effect/jHelpers/Hero.swift
@@ -16,9 +16,42 @@ struct HeroWrapper<Content: View>: View {
     
     var body: some View {
         content
+            .customOnChange(value: scene) { newValue in
+                if newValue == .active { addOverlayWindow() }
+            }
+    }
+
+    /// adding overlay window
+    func addOverlayWindow() {
+        for scene in UIApplication.shared.connectedScenes {
+            /// finding active scene
+            if let windowScene = scene as? UIWindowScene, scene.activationState == .foregroundActive, overlayWindow == nil {
+                let window = UIWindow(windowScene: windowScene)
+                window.backgroundColor = .clear
+                window.isUserInteractionEnabled = false
+                window.isHidden = false
+                let rootController = UIHostingController(rootView: HeroLayerView())
+                rootController.view.frame = windowScene.screen.bounds
+                rootController.view.backgroundColor = .clear
+
+                window.rootViewController = rootController
+                self.overlayWindow = window
+            }
+        }
+
+        if overlayWindow == nil {
+
+        }
     }
 }
 
 #Preview {
     UHEContentView()
+}
+
+fileprivate struct HeroLayerView: View {
+    var body: some View {
+        Rectangle()
+            .fill(.red)
+    }
 }
