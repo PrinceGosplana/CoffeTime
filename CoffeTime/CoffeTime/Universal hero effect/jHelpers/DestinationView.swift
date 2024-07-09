78197f90377b970ea2c4479923884967c4a53675
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-09 19:39:50 +0300

    add DestinationView

--- CoffeTime/CoffeTime/Universal hero effect/jHelpers/DestinationView.swift
+++ CoffeTime/CoffeTime/Universal hero effect/jHelpers/DestinationView.swift
@@ -5,4 +5,11 @@
 //  Created by Oleksandr Isaiev on 09.07.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct DestinationView<Content: View>: View {
+    @ViewBuilder var content: Content
+    var body: some View {
+        content
+    }
+}
