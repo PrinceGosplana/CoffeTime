3a3c8dce1b3a0c138715e3cd2c33f5bc6c728dc9
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-13 13:21:33 +0300

    prepare base ui for mocks

--- CoffeTime/CoffeTime/Universal hero effect/UHEContentView.swift
+++ CoffeTime/CoffeTime/Universal hero effect/UHEContentView.swift
@@ -7,51 +7,65 @@
 
 import SwiftUI
 
+//struct UHEContentView: View {
+//    @State private var showView: Bool = false
+//
+//    var body: some View {
+//        NavigationStack {
+//            VStack {
+//                SourceView(id: "View 1") {
+//                    Circle()
+//                        .fill(.red)
+//                        .frame(width: 50, height: 50)
+//                        .onTapGesture {
+//                            showView.toggle()
+//                        }
+//                }
+//            }
+//            .padding()
+//            .navigationTitle("Hero")
+//            .navigationDestination(isPresented: $showView) {
+//
+//                DestinationView(id: "View 1") {
+//                    Circle()
+//                        .fill(.red)
+//                        .frame(width: 150, height: 150)
+//                        .onTapGesture {
+//                            showView.toggle()
+//                        }
+//                }
+//                .padding(15)
+//                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+//                .navigationBarBackButtonHidden()
+//                .navigationTitle("Detail View")
+//            }
+//        }
+//        .heroLayer(
+//            id: "View 1",
+//            animate: $showView) {
+//                Circle()
+//                    .fill(.red)
+//            } completion: { status in
+//
+//            }
+//
+//    }
+//}
+
 struct UHEContentView: View {
     @State private var showView: Bool = false
 
     var body: some View {
         NavigationStack {
-            VStack {
-                SourceView(id: "View 1") {
-                    Circle()
-                        .fill(.red)
-                        .frame(width: 50, height: 50)
-                        .onTapGesture {
-                            showView.toggle()
-                        }
-                }
-            }
-            .padding()
-            .navigationTitle("Hero")
-            .navigationDestination(isPresented: $showView) {
+            List {
+                ForEach(HeroItem.mocks) { item in
 
-                DestinationView(id: "View 1") {
-                    Circle()
-                        .fill(.red)
-                        .frame(width: 150, height: 150)
-                        .onTapGesture {
-                            showView.toggle()
-                        }
                 }
-                .padding(15)
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
-                .navigationBarBackButtonHidden()
-                .navigationTitle("Detail View")
             }
+            .navigationTitle("Hero Effect")
         }
-        .heroLayer(
-            id: "View 1",
-            animate: $showView) {
-                Circle()
-                    .fill(.red)
-            } completion: { status in
-
-            }
-
     }
 }
-
 #Preview {
     UHEContentView()
 }
