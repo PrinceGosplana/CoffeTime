68eb7dfe708a2026b102bc91eb59d6ba855bf330
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-13 14:04:35 +0300

    base ui implementation

---
+++ CoffeTime/CoffeTime/Progress based hero animation effect/View/HAEContentVeiw.swift
@@ -0,0 +1,18 @@
+//
+//  HAEContentVeiw.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 13.07.2024.
+//
+
+import SwiftUI
+
+struct HAEContentVeiw: View {
+    var body: some View {
+        HAEHomeView()
+    }
+}
+
+#Preview {
+    HAEContentVeiw()
+}
---
+++ CoffeTime/CoffeTime/Progress based hero animation effect/View/HAEHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HAEHomeView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 13.07.2024.
+//
+
+import SwiftUI
+
+struct HAEHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HAEContentVeiw()
+}
