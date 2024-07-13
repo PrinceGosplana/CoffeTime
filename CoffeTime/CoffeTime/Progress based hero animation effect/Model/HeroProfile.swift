aea7d29d91f1c84790b4861b1567391fcfa43dc3
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-13 14:02:27 +0300

    add model and mock data

---
+++ CoffeTime/CoffeTime/Progress based hero animation effect/Model/HeroProfile.swift
@@ -0,0 +1,22 @@
+//
+//  HeroProfile.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 13.07.2024.
+//
+
+import SwiftUI
+
+struct HeroProfile: Identifiable {
+    var id = UUID()
+    let userName: String
+    let profilePicture: String
+    let lastMsg: String
+}
+
+extension HeroProfile {
+    static let mocks: [HeroProfile] = [
+        .init(userName: "Henna Ezarik", profilePicture: "girl_1", lastMsg: "I miss you"),
+        .init(userName: "Julie Stark", profilePicture: "girl_2", lastMsg: "I'm watching at you!")
+    ]
+}
