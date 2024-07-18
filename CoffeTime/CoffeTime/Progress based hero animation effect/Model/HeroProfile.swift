af74faeabc8c3ddb18f9f604a34c0d491afe398b
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-18 15:52:00 +0300

    add resources

--- CoffeTime/CoffeTime/Progress based hero animation effect/Model/HeroProfile.swift
+++ CoffeTime/CoffeTime/Progress based hero animation effect/Model/HeroProfile.swift
@@ -18,6 +18,10 @@ extension HeroProfile {
     static let mocks: [HeroProfile] = [
         .init(userName: "Henna Ezarik", profilePicture: "girl_1", lastMsg: "I miss you"),
         .init(userName: "Julie Stark", profilePicture: "girl_2", lastMsg: "I'm watching at you!"),
-        .init(userName: "Janet Groove", profilePicture: "girl_3", lastMsg: "Shame on you!")
+        .init(userName: "Janet Groove", profilePicture: "girl_3", lastMsg: "Shame on you!"),
+        .init(userName: "Maria", profilePicture: "girl_4", lastMsg: "I'll miss you"),
+        .init(userName: "Amber Clott", profilePicture: "girl_5", lastMsg: "Come to me"),
+        .init(userName: "Caroline Ant", profilePicture: "girl_6", lastMsg: "No need apology"),
+        .init(userName: "Isolda R.", profilePicture: "girl_7", lastMsg: "Youth and lust")
     ]
 }
