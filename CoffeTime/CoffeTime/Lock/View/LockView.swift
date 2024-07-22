828036c372e79807ca4d757b8d122c2cfcc5823c
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-22 15:25:55 +0300

    moved NumberPadPin into main file

--- CoffeTime/CoffeTime/Lock/View/LockView.swift
+++ CoffeTime/CoffeTime/Lock/View/LockView.swift
@@ -12,10 +12,12 @@ struct LockView<Content: View>: View {
     let lockType: LockType
     let lockPin: String
     let isEnabled: Bool
+    let maxLimit = 4
     var lockWhenAppGoesBackground: Bool = true
     @State private var pin: String = ""
     @State private var isUnlocked: Bool = false
     @State private var noBiometricAccess: Bool = false
+    @State private var animateField: Bool = false
 
     var forgotPin: () -> () = { }
     @ViewBuilder let content: Content
@@ -33,7 +35,7 @@ struct LockView<Content: View>: View {
                     Rectangle()
                         .fill(.black)
                         .ignoresSafeArea()
-                    if lockType == .both || lockType == .biometric {
+                    if (lockType == .both && !noBiometricAccess) || lockType == .biometric {
                         Group {
                             if noBiometricAccess {
                                 Text("Enable biometric authentication in Settings to unlock the view.")
@@ -55,7 +57,7 @@ struct LockView<Content: View>: View {
                                     .background(.ultraThinMaterial, in: .rect(cornerRadius: 10))
                                     .contentShape(.rect)
                                     .onTapGesture {
-
+                                        unlockView()
                                     }
 
                                     if lockType == .both {
@@ -64,7 +66,7 @@ struct LockView<Content: View>: View {
                                             .background(.ultraThinMaterial, in: .rect(cornerRadius: 10))
                                             .contentShape(.rect)
                                             .onTapGesture {
-                                                
+                                                noBiometricAccess = true
                                             }
                                     }
                                 }
@@ -72,7 +74,7 @@ struct LockView<Content: View>: View {
                         }
                     } else {
                         /// Custom number pad to type view lock pin
-                        NumberPadPin(forgotPin: forgotPin, pin: $pin, lockPin: lockPin, isUnlocked: $isUnlocked)
+                        NumberPadPin()
                     }
                 }
                 .environment(\.colorScheme, .dark)
@@ -81,11 +83,136 @@ struct LockView<Content: View>: View {
         }
     }
 
+    private func unlockView() {
+
+    }
+
     /// Lock Type
     enum LockType: String {
         case biometric = "Bio Metric Auth"
         case number = "Custom Number Lock"
         case both = "First preference will be biometric, and if it's not available, it will go for number lock"
+    }
+
+    @ViewBuilder
+    private func NumberPadPin() -> some View {
+            VStack(spacing: 15) {
+                Text("Enter Pin")
+                    .font(.title.bold())
+                    .frame(maxWidth: .infinity)
+                    .overlay(alignment: .leading) {
+                        /// back button only for both lock type
+
+                    }
+
+                /// adding wiggling animation for wrong password with keyframe animation
+                HStack(spacing: 10) {
+                    ForEach(0..<4, id:\.self) { index in
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 50, height: 55)
+                        /// Showing Pin at each box with the help of index
+                            .overlay {
+                                /// Safe check
+                                if pin.count > index {
+                                    let index = pin.index(pin.startIndex, offsetBy: index)
+                                    let string = String(pin[index])
+
+                                    Text(string)
+                                        .font(.title.bold())
+                                        .foregroundStyle(.black)
+                                }
+                            }
+                    }
+                }
+                .keyframeAnimator(initialValue: CGFloat.zero, trigger: animateField, content: { content, value in
+                    content
+                        .offset(x: value)
+                }, keyframes: { _ in
+                    CubicKeyframe(30, duration: 0.07)
+                    CubicKeyframe(-30, duration: 0.07)
+                    CubicKeyframe(20, duration: 0.07)
+                    CubicKeyframe(-20, duration: 0.07)
+                    CubicKeyframe(0, duration: 0.07)
+                })
+                .padding(.top, 15)
+                .overlay(alignment: .bottomTrailing) {
+                    Button("Forgot Pin?") {
+                        forgotPin()
+                    }
+                    .foregroundStyle(.white)
+                    .offset(y: 40)
+                }
+                .frame(maxHeight: .infinity)
+
+                /// Custom number pad
+                GeometryReader { _ in
+                    LazyVGrid(columns: Array(repeating: GridItem(), count: 3), content: {
+                        ForEach(1...9, id:\.self) { number in
+                            Button {
+                                /// Adding number to pin
+                                /// Max limit - 4
+                                if pin.count < maxLimit {
+                                    pin.append("\(number)")
+                                }
+                            } label: {
+                                Text("\(number)")
+                                    .font(.title)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.vertical, 20)
+                                    .contentShape(.rect)
+                            }
+                            .tint(.white)
+                        }
+
+                        Button {
+                            if !pin.isEmpty {
+                                pin.removeLast()
+                            }
+                        } label: {
+                            Image(systemName: "delete.backward")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 20)
+                                .contentShape(.rect)
+                        }
+                        .tint(.white)
+
+                        Button {
+                            if pin.count <= maxLimit {
+                                pin.append("0")
+                            }
+                        } label: {
+                            Text("0")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .padding(.vertical, 20)
+                                .contentShape(.rect)
+                        }
+                        .tint(.white)
+
+                    })
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                }
+                .onChange(of: pin) { oldValue, newValue in
+                    if newValue.count == maxLimit {
+                        /// Validate pin
+                        if lockPin == pin {
+                            withAnimation(.snappy, completionCriteria: .logicallyComplete) {
+                                isUnlocked = true
+                            } completion: {
+                                /// clearing pin
+                                pin = ""
+                            }
+                        } else {
+                            pin = ""
+                            animateField.toggle()
+                        }
+                    }
+                }
+
+            }
+            .padding()
+            .environment(\.colorScheme, .dark)
     }
 }
 
