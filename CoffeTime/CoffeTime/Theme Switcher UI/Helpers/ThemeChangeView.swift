c37975307f4e208942098b79277f33bf4093a1b0
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-07-30 11:32:36 +0300

    base init

---
+++ CoffeTime/CoffeTime/Theme Switcher UI/Helpers/ThemeChangeView.swift
@@ -0,0 +1,22 @@
+//
+//  ThemeChangeView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 30.07.2024.
+//
+
+import SwiftUI
+
+struct ThemeChangeView: View {
+    @Environment(\.colorScheme) private var scheme
+
+    var body: some View {
+        VStack(spacing: 15) {
+            
+        }
+    }
+}
+
+#Preview {
+    ThemeChangeView()
+}
