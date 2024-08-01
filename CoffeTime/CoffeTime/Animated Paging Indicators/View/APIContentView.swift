46b3a37c1c45167b3043e83dcee603cf7c58ea80
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-01 16:11:47 +0300

    base ui preparation for animated paging indicators

---
+++ CoffeTime/CoffeTime/Animated Paging Indicators/View/APIContentView.swift
@@ -0,0 +1,20 @@
+//
+//  APIContentView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 01.08.2024.
+//
+
+import SwiftUI
+
+struct APIContentView: View {
+    var body: some View {
+        NavigationStack {
+            APIHome()
+        }
+    }
+}
+
+#Preview {
+    APIContentView()
+}
---
+++ CoffeTime/CoffeTime/Animated Paging Indicators/View/APIHome.swift
@@ -0,0 +1,20 @@
+//
+//  APIHome.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 01.08.2024.
+//
+
+import SwiftUI
+
+struct APIHome: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    APIContentView()
+}
