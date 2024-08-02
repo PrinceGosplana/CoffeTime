2cf61c8a6d36c56d0fc484f7022044546fce3735
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-02 14:42:17 +0300

    base ui implementation for cover flow

---
+++ CoffeTime/CoffeTime/Cover Flow/CoverFlowContentView.swift
@@ -0,0 +1,24 @@
+//
+//  CoverFlowContentView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 02.08.2024.
+//
+
+import SwiftUI
+
+struct CoverFlowContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+
+            }
+            .navigationTitle("CoverFlow")
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    CoverFlowContentView()
+}
---
+++ CoffeTime/CoffeTime/Cover Flow/Helpers/CoverFlowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoverFlowView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 02.08.2024.
+//
+
+import SwiftUI
+
+struct CoverFlowView<Content: View>: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoverFlowContentView()
+}
