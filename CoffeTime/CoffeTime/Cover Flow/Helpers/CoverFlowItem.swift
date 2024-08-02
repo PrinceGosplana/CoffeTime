1871d68b7ae4382103d44b31b7dd2e7453bacaa1
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-02 14:43:51 +0300

    CoverFlowItem implemented

---
+++ CoffeTime/CoffeTime/Cover Flow/Helpers/CoverFlowItem.swift
@@ -0,0 +1,13 @@
+//
+//  CoverFlowItem.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 02.08.2024.
+//
+
+import SwiftUI
+
+struct CoverFlowItem: Identifiable {
+    let id: UUID = .init()
+    var color: Color
+}
