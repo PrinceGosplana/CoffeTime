f5a382fb62b8cf1feb5dea5d292fd6edc92a7335
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-16 09:58:28 +0300

    search bar - dummy message view

---
+++ CoffeTime/CoffeTime/Search bar/DummyMessageView.swift
@@ -0,0 +1,32 @@
+//
+//  DummyMessageView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 16.08.2024.
+//
+
+import SwiftUI
+
+struct DummyMessageView: View {
+    var body: some View {
+        HStack(spacing: 12) {
+            Circle()
+                .frame(width: 50, height: 50)
+            VStack(alignment: .leading) {
+                Rectangle()
+                    .frame(width: 140, height: 5)
+
+                Rectangle()
+                    .frame(height: 10)
+                    .frame(maxWidth: .infinity)
+                    .clipShape(RoundedRectangle(cornerRadius: 3))
+            }
+        }
+        .foregroundStyle(.gray.opacity(0.4))
+        .padding(.horizontal, 15)
+    }
+}
+
+#Preview {
+    DummyMessageView()
+}
