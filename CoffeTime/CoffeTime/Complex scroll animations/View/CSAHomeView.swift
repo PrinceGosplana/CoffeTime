850998ecce8271aad22b000c2b4a0bfdcd7fe603
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-19 10:14:19 +0300

    main ui elements

--- CoffeTime/CoffeTime/Complex scroll animations/View/CSAHomeView.swift
+++ CoffeTime/CoffeTime/Complex scroll animations/View/CSAHomeView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct CSAHomeView: View {
+
+    /// View Properties
+    @State private var allExpenses: [Expense] = []
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing: 0) {
+                VStack(alignment: .leading, spacing: 15) {
+                    Text("Hello there!")
+                        .font(.largeTitle.bold())
+                        .padding(.horizontal, 15)
+
+                    GeometryReader {
+                        let rect = $0.frame(in: .scrollView)
+                    }
+                    .frame(height: 125)
+                }
+                LazyVStack(spacing: 15) {
+                    Menu {
+
+                    } label: {
+                        HStack(spacing: 4) {
+                            Text("Filter By")
+                            Image(systemName: "chevron.down")
+                        }
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+
+                    ForEach(Expense.expenses) { expense in
+                        
+                    }
+                }
+                .padding(15)
+            }
+            .padding(.vertical, 15)
+        }
     }
 }
 
