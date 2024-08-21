320748359dbc2724f5bfe74c46276419ebd618b3
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-21 07:53:03 +0300

    add draggable modifier to grids

--- CoffeTime/CoffeTime/Movable grids/MovableGridsView.swift
+++ CoffeTime/CoffeTime/Movable grids/MovableGridsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MovableGridsView: View {
 
     @State private var colors: [Color] = [.red, .blue, .purple, .yellow, .black, .indigo, .cyan, .brown, .mint, .orange]
+    @State private var draggingItem: Color?
 
     var body: some View {
         NavigationStack {
@@ -17,10 +18,21 @@ struct MovableGridsView: View {
                 let columns = Array(repeating: GridItem(spacing: 10), count: 3)
 
                 LazyVGrid(columns: columns, spacing: 10) {
-                    ForEach(colors, id:\.self) {
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .fill($0.gradient)
-                            .frame(height: 100)
+                    ForEach(colors, id:\.self) { color in
+                        GeometryReader {
+                            let size = $0.size
+
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                .fill(color.gradient)
+                            /// we can pass any item that conforms to the transferable protocol, such as a string, Data, etc., in the draggable modifier
+                                .draggable(color) {
+                                    /// Custom preview view
+                                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                                        .fill(.ultraThinMaterial)
+                                        .frame(width: size.width, height: size.height)
+                                }
+                        }
+                        .frame(height: 100)
                     }
                 }
                 .padding(15)
