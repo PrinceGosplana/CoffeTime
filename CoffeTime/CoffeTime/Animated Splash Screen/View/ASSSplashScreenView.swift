1e66f8404a38c316173ffb823bf5659b76cfea66
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-04 13:43:23 +0300

    base screens for animated splash screen

---
+++ CoffeTime/CoffeTime/Animated Splash Screen/View/ASSSplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  ASSSplashScreenView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 04.09.2024.
+//
+
+import SwiftUI
+
+struct ASSSplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ASSSplashScreenView()
+}
