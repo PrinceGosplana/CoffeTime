767e51083646c18e68bc088a9fb9d1f89d0b2524
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-15 09:26:51 +0300

    prepare base ui for animated carousel

---
+++ CoffeTime/CoffeTime/Animated Carousel/ACContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ACContentView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 15.09.2024.
+//
+
+import SwiftUI
+
+struct ACContentView: View {
+    var body: some View {
+        ACHomeView()
+    }
+}
+
+#Preview {
+    ACContentView()
+}
---
+++ CoffeTime/CoffeTime/Animated Carousel/ACHomeView.swift
@@ -0,0 +1,38 @@
+//
+//  ACHomeView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 15.09.2024.
+//
+
+import SwiftUI
+
+struct ACHomeView: View {
+
+    @State private var currentTab = "For you"
+
+    /// Hiding tabbar
+    init() {
+        UITabBar.appearance().isHidden = true
+    }
+
+    var body: some View {
+        TabView(selection: $currentTab) {
+            Text("For you")
+                .tag("For you")
+
+            Text("Search")
+                .tag("Search")
+
+            Text("Following")
+                .tag("Following")
+
+            Text("Downloads")
+                .tag("Downloads")
+        }
+    }
+}
+
+#Preview {
+    ACContentView()
+}
