df71d20bb785927ed5c951f10e6ed1fae3b5fd7c
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-17 14:50:15 +0300

    base ui for full swipe to pop view project

---
+++ CoffeTime/CoffeTime/Full Swipe to pop view/View/FSPVContentView.swift
@@ -0,0 +1,18 @@
+//
+//  FSPVContentView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 17.09.2024.
+//
+
+import SwiftUI
+
+struct FSPVContentView: View {
+    var body: some View {
+        FSPVHomeView()
+    }
+}
+
+#Preview {
+    FSPVContentView()
+}
---
+++ CoffeTime/CoffeTime/Full Swipe to pop view/View/FSPVHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  FSPVHomeView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 17.09.2024.
+//
+
+import SwiftUI
+
+struct FSPVHomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    FSPVContentView()
+}
