cbe3a7a0776286e0fd72753dd83e939b0822f7cf
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-18 09:58:16 +0300

    base ui preparation for collapsable animated sticky header

---
+++ CoffeTime/CoffeTime/Collapsable Animated Sticky Header/View/CASHContentView.swift
@@ -0,0 +1,18 @@
+//
+//  CASHContentView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 18.09.2024.
+//
+
+import SwiftUI
+
+struct CASHContentView: View {
+    var body: some View {
+        CASHHomeView()
+    }
+}
+
+#Preview {
+    CASHContentView()
+}
---
+++ CoffeTime/CoffeTime/Collapsable Animated Sticky Header/View/CASHHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  CASHHomeView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 18.09.2024.
+//
+
+import SwiftUI
+
+struct CASHHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CASHContentView()
+}
