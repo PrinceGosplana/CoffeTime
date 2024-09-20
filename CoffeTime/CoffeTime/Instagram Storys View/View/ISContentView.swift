954baa71fbfd7eee66d8b00d8a629922bf74d1a1
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-20 10:30:08 +0300

    instagram story with timer and 3d animation base state

---
+++ CoffeTime/CoffeTime/Instagram Storys View/View/ISContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ISContentView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 20.09.2024.
+//
+
+import SwiftUI
+
+struct ISContentView: View {
+    var body: some View {
+        ISHomeView()
+    }
+}
+
+#Preview {
+    ISContentView()
+}
---
+++ CoffeTime/CoffeTime/Instagram Storys View/View/ISHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  ISHomeView.swift
+//  CoffeTime
+//
+//  Created by Oleksandr Isaiev on 20.09.2024.
+//
+
+import SwiftUI
+
+struct ISHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ISContentView()
+}
