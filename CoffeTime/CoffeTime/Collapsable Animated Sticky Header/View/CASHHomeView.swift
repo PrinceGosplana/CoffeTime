92392f6e6a1cbd256863388d2b4e84a33a4477fa
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-20 09:59:37 +0300

    dynamic corner radius

--- CoffeTime/CoffeTime/Collapsable Animated Sticky Header/View/CASHHomeView.swift
+++ CoffeTime/CoffeTime/Collapsable Animated Sticky Header/View/CASHHomeView.swift
@@ -28,7 +28,7 @@ struct CASHHomeView: View {
                         .frame(height: getHeaderHeight(), alignment: .bottom)
                         .background(
                             Color.appYellow ?? .yellow,
-                            in: CASHCustomCorner(corners: [.bottomRight], radius: 50)
+                            in: CASHCustomCorner(corners: [.bottomRight], radius: getCornerRadius())
                         )
                         .overlay(
                             /// top nav view
@@ -50,8 +50,9 @@ struct CASHHomeView: View {
                                 }
                             }
                                 .padding(.horizontal)
-                                .frame(height: 80 + topEdge)
+                                .frame(height: 60)
                                 .foregroundStyle(.white)
+                                .padding(.top, topEdge)
                             , alignment: .top
 
                         )
@@ -76,6 +77,13 @@ struct CASHHomeView: View {
     private func getHeaderHeight() -> CGFloat {
         let topHeight = maxHeight + offset
         return topHeight > (80 + topEdge) ? topHeight : (80 + topEdge)
+    }
+
+    private func getCornerRadius() -> CGFloat {
+        let progress = -offset / (maxHeight - (80 + topEdge))
+        let value = 1 - progress
+        let radius = value  * 50
+        return offset < 0 ? radius : 50
     }
 }
 
