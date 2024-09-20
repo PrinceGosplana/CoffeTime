7ed25390fb87985a82f8677c02999e4d669b7b9d
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-20 10:44:36 +0300

    base ui for home view and resources

--- CoffeTime/CoffeTime/Instagram Storys View/View/ISHomeView.swift
+++ CoffeTime/CoffeTime/Instagram Storys View/View/ISHomeView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct ISHomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        Button {
+
+                        } label: {
+                            Image(.instagram128)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 60, height: 60)
+                                .overlay(
+                                    Image(systemName: "plus")
+                                        .padding(4)
+                                        .background(.blue, in: Circle())
+                                        .foregroundStyle(.white)
+                                        .padding(2)
+                                        .background(.white, in: Circle())
+                                        .offset(x: 8, y: 6)
+                                    , alignment: .bottomTrailing
+                                )
+                        }
+                    }
+                    .padding()
+                    .padding(.top, 10)
+                }
+            }
+            .navigationTitle("Instagram")
+        }
     }
 }
 
