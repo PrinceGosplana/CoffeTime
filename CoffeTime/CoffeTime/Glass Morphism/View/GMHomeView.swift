045c1f5094bbc22aa561cb49e5951b347b1f7450
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-25 18:05:11 +0300

    base ui elements

--- CoffeTime/CoffeTime/Glass Morphism/View/GMHomeView.swift
+++ CoffeTime/CoffeTime/Glass Morphism/View/GMHomeView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct GMHomeView: View {
+
+    let subtitle = "Start now and learn more about \n local weather instantly"
     var body: some View {
         ZStack {
             LinearGradient(
@@ -58,7 +60,67 @@ struct GMHomeView: View {
                     .blur(radius: 110)
                     .offset(x: -size.width / 1.8, y: size.height / 2)
             }
+
+            /// Main content
+            VStack {
+
+                Spacer(minLength: 10)
+                
+                Text("Known Everything\nabout the weather")
+                    .font(.system(size: 40, weight: .bold))
+
+                Text(getAttributedString())
+                    .fontWeight(.semibold)
+                    .kerning(1.1)
+                    .padding(.top, 10)
+
+                Button {
+
+                } label: {
+                    Text("Get Started")
+                        .font(.title.bold())
+                        .padding(.vertical, 22)
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            .linearGradient(
+                                colors: [
+                                    Color("AppBrown"),
+                                    Color("AppLightBrown")
+                                ],
+                                startPoint: .leading,
+                                endPoint: .trailing
+                            )
+                            ,
+                            in: RoundedRectangle(cornerRadius: 20)
+                        )
+                }
+                .padding(.horizontal, 50)
+                .padding(.vertical, 20)
+
+                Button {
+
+                } label: {
+                    Text("Already have an account?")
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.gray)
+                }
+            }
+            .foregroundStyle(.white)
+            .multilineTextAlignment(.center)
+            .padding()
         }
+    }
+
+    /// AttributedString from iOS 15
+    private func getAttributedString() -> AttributedString {
+        var attStr = AttributedString(subtitle)
+        attStr.foregroundColor = .gray
+
+        /// converting only 'local weather'
+        if let range = attStr.range(of: "local weather") {
+            attStr[range].foregroundColor = .white
+        }
+        return attStr
     }
 }
 
