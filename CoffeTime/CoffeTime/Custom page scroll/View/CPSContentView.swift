60730170ecbb60c7fcea8c0eb15c606ce94d799b
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-08 18:30:45 +0300

    base ui for custom page scroll

---
+++ CoffeTime/CoffeTime/Custom page scroll/View/CPSContentView.swift
@@ -0,0 +1,18 @@
+//
+//  CPSContentView.swift
+//  CoffeTime
+//
+//  Created by OLEKSANDR ISAIEV on 08.10.2024.
+//
+
+import SwiftUI
+
+struct CPSContentView: View {
+    var body: some View {
+        CPSHomeView()
+    }
+}
+
+#Preview {
+    CPSContentView()
+}
---
+++ CoffeTime/CoffeTime/Custom page scroll/View/CPSHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  CPSHomeView.swift
+//  CoffeTime
+//
+//  Created by OLEKSANDR ISAIEV on 08.10.2024.
+//
+
+import SwiftUI
+
+struct CPSHomeView: View {
+    var body: some View {
+        Text("CPS Home View")
+    }
+}
+
+#Preview {
+    CPSContentView()
+}
