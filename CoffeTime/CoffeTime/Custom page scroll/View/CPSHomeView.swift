dfdaf9062d26a02a26fbc77ce6cc4a54295e4df1
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-08 18:54:33 +0300

    add standView

--- CoffeTime/CoffeTime/Custom page scroll/View/CPSHomeView.swift
+++ CoffeTime/CoffeTime/Custom page scroll/View/CPSHomeView.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct CPSHomeView: View {
+    
+    private var titleString: AttributedString {
+        var attString = AttributedString(stringLiteral: "My Library")
+        if let range = attString.range(of: "Library") {
+            attString[range].font = .largeTitle.bold()
+        }
+        return attString
+    }
+    
+    var standView: some View {
+        Rectangle()
+            .fill(.white.opacity(0.6))
+            .shadow(color: .black.opacity(0.6), radius: 20, x: 0, y: 5)
+            .frame(height: 10)
+            .overlay(alignment: .top) {
+                Rectangle()
+                    .fill(.white.opacity(0.75).gradient)
+                    .frame(height: 385)
+                /// 3D Rotation
+                    .rotation3DEffect(.init(degrees: -98), axis: (x: 1, y: 0, z: 0), anchor: .top, anchorZ: 0.5, perspective: 1)
+                    .shadow(color: .black.opacity(0.08), radius: 25, x: 0, y: 5)
+                    .shadow(color: .black.opacity(0.08), radius: 5, x: 0, y: 15)
+            }
+    }
+    
     var body: some View {
-        Text("CPS Home View")
+        VStack {
+            Text(titleString)
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                standView
+            }
+            .padding(.horizontal, -15)
+            .frame(height: 250)
+        }
+        .padding(15)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .background(Color("BG"), ignoresSafeAreaEdges: .all)
     }
 }
 
